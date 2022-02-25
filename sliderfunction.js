c3bf33e92c2c2b6ecb12f1922703f28061b7f5ac
Al-Balone <85862719+Al-Balone@users.noreply.github.com>
2022-02-25 14:02:30 +0000

    update url functionality

--- sliderfunction.js
+++ sliderfunction.js
@@ -7,26 +7,34 @@ const blurSlider = document.getElementById("blurSlider");
 const saturateSlider = document.getElementById("saturateSlider");
 const sepiaSlider = document.getElementById("sepiaSlider");
 
+const image = document.getElementById("mainImage");
+const theForm = document.getElementById("image_form");
+
+const updateImage = (event) => {
+    event.preventDefault();
+    const input = document.getElementById('url_input').value
+    image.src = input;
+    }
+
+theForm.addEventListener("submit", updateImage, true);
+
 cornerSlider.addEventListener("input", (e) => {
     root.style.setProperty("--radius", `${e.target.value}%`);
 });
-
 graySlider.addEventListener("input", (e) => {
     root.style.setProperty("--gray", `${e.target.value}%`);
 });
-
 brightSlider.addEventListener("input", (e) => {
     root.style.setProperty("--bright", `${e.target.value}%`);
 });
-
 blurSlider.addEventListener("input", (e) => {
     root.style.setProperty("--blur", `${e.target.value}px`);
 });
-
 saturateSlider.addEventListener("input", (e) => {
     root.style.setProperty("--saturate", `${e.target.value}%`);
 });
-
 sepiaSlider.addEventListener("input", (e) => {
     root.style.setProperty("--sepia", `${e.target.value}%`);
 });
+
+
